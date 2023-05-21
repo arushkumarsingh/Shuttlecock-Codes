bd67d25e5702e389fccaa1eb064a8fa63c89c1a3
Arush Kumar Singh <61361398+arushkumarsingh@users.noreply.github.com>
2023-05-21 15:12:27 +0530

    Delete sortm.m

--- sortm.m
+++
@@ -1,8 +0,0 @@
-%function for matrix sorting 
-function[sortedMatrix] = sortm(cell)
-    matr = zeros(2,10);
-    matr(1,:) = cell2mat(cell(:,1));
-    matr(2,:) = cell2mat(cell(:,2));
-    [sortedRow, sortedIndices] = sort(matr(2, :));
-    sortedMatrix = matr(:, sortedIndices);
-end
